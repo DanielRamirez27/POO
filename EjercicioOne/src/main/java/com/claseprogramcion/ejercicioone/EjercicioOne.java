b346f9a2a23980807c66ad2abada1d6df8d7c6f9
DanielRamirez27 <ramirezgaraydaniel@gmail.com>
2024-02-29 22:02:58 +0100

    frase de los simpson two

--- EjercicioOne/src/main/java/com/claseprogramcion/ejercicioone/EjercicioOne.java
+++ EjercicioOne/src/main/java/com/claseprogramcion/ejercicioone/EjercicioOne.java
@@ -1,33 +1,22 @@
 package com.claseprogramcion.ejercicioone;
 
+import people.Character;
+
 public class EjercicioOne {
 
     public static void main(String[] args) {
 
-        Character homero = new Character("Homero", 39, 'M', "a la grande le puse cuca");
+        Character homero = new Character("Homero", 39, 'M', "A la grande le puse cuca");
         homero.saySomething();
 
-        Character bart = new Character("Bartolomeo", 10, 'M', "ay caramba");
+        Character bart = new Character("Bartolomeo", 10, 'M', "Ay caramba!!");
         bart.saySomething();
+        
+        Character apu = new Character();
+        apu.setName("Apu");
+        apu.setPhrase("vuelvas prontos");
+        
+        apu.saySomething();
 
-    }
-
-    static class Character {
-
-        String name;
-        int age;
-        char gender;
-        String phrase;
-
-        public Character(String name, int age, char gender, String phrase) {
-            this.name = name;
-            this.age = age;
-            this.gender = gender;
-            this.phrase = phrase;
-        }
-
-        void saySomething() {
-            System.out.println(this.phrase);
-        }
     }
 }
